695afd33239fb2304796e4cb7ce3e37f3fe4c822
Paul Stadig <paul@stadig.name>
2013-04-30 06:43:28 -0400

    Initial project structure.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ledger.import "0.1.0-SNAPSHOT"
+  :description "A little bit of code for importing my personal banking
+  transactions into ledger."
+  :url "http://github.com/pjstadig/ledger.import/"
+  :license {:name "Mozilla Public License, v. 2.0"
+            :url "http://mozilla.org/MPL/2.0/"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/ledger/import.clj
@@ -0,0 +1,23 @@
+(ns ledger.import
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:import (java.text SimpleDateFormat)))
+
+(defn parse-csv [f]
+  (let [parser (SimpleDateFormat. "MM/dd/yyyy")]
+    (for [line (str/split-lines (slurp f))
+          :let [[_ _ date _ description category amount] (str/split line #",")]]
+      {:date (.parse parser date)
+       :description description
+       :category category
+       :amount (bigdec (if (= \" (first amount))
+                         (.substring amount 1 (dec (count amount)))
+                         amount))})))
+
+(defn write-ledger [out entries]
+  (let [formatter (SimpleDateFormat. "yyyy-MM-dd")]
+   (with-open [out (io/writer out)]
+     (doseq [{:keys [date description category amount]} entries]
+       (.write out (str (.format formatter date) " " description "\n"))
+       (.write out (str "\tUSAA Checking\t" amount "\n"))
+       (.write out (str "\t" category "\n"))))))
